46bd0b23d38f7d779e6392a6c8ad075d02de8e52
MaxWasTaked <61461142+Yonaga678@users.noreply.github.com>
2021-04-02 12:57:43 +0300

    02.04.2021 12:57

--- javascript.js
+++ javascript.js
@@ -25,10 +25,40 @@ window.onclick = function(event) {
 }
 
 //Account creating!
-var First = document.getElementById("First");
-
-var Last = document.getElementById("Last");
-
-var Phone = document.getElementById("Phone");
-
 var Check = document.getElementById("Check");
+
+Check.onclick = function(event) {
+  console.clear()
+  console.log(`Check button is working!`)
+}
+
+
+
+// Set the date we're counting down to
+var countDownDate = new Date("Jan 5, 2022 15:37:25").getTime();
+
+// Update the count down every 1 second
+var x = setInterval(function() {
+
+  // Get today's date and time
+  var now = new Date().getTime();
+    
+  // Find the distance between now and the count down date
+  var distance = countDownDate - now;
+    
+  // Time calculations for days, hours, minutes and seconds
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+    
+  // Output the result in an element with id="demo"
+  document.getElementById("demo").innerHTML = days + "D " + hours + "H "
+  + minutes + "M " + seconds + "S ";
+    
+  // If the count down is over, write some text 
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("demo").innerHTML = "EXPIRED";
+  }
+}, 1000);
