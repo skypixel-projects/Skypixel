090d0ec809b161b046a9a41b3da087140885a7a4
MaxWasTaked <61461142+Yonaga678@users.noreply.github.com>
2021-04-02 22:36:36 +0300

    02.04.2021 22:36

--- javascript.js
+++ javascript.js
@@ -35,7 +35,7 @@ Check.onclick = function(event) {
 
 
 // Set the date we're counting down to
-var countDownDate = new Date("Jan 5, 2022 15:37:25").getTime();
+var countDownDate = new Date("Jan 1, 2022 00:00:00").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function() {
