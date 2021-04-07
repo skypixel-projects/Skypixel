dff4b5403585a0e257f0dc09c69602a42b538723
MaxWasTaked <61461142+Yonaga678@users.noreply.github.com>
2021-04-08 00:25:43 +0300

    08.04.2021 12:25
    Fixed some bugs on web load. Added new 404 web error!

--- javascript.js
+++ javascript.js
@@ -17,3 +17,20 @@ function topFunction() {
   document.body.scrollTo({top: 0, behavior: 'smooth'}); // For Safari
   document.documentElement.scrollTo({top: 0, behavior: 'smooth'}); // For Chrome, Firefox, IE and Opera
 }
+
+function myFunction() {
+  document.getElementById("demo").innerHTML = "OPENING";
+  setTimeout(
+    function() {
+      document.getElementById("demo").innerHTML = "LOGIN";
+      window.location.href = 'pages/404/index.html'
+  }, 1000);
+}
+
+function loadTop() {
+  setTimeout(
+    function() {
+      document.body.scrollTo({top: 0, behavior: 'smooth'}); // For Safari
+      document.documentElement.scrollTo({top: 0, behavior: 'smooth'}); // For Chrome, Firefox, IE and Opera
+  }, 100);
+}
