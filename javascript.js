d5bf788454c0eb05733a10e42106b38b8ad31b86
MaxWasTaked <61461142+Yonaga678@users.noreply.github.com>
2021-04-09 13:01:50 +0300

    09.04.2021 1:01
    New update

--- javascript.js
+++ javascript.js
@@ -18,19 +18,67 @@ function topFunction() {
   document.documentElement.scrollTo({top: 0, behavior: 'smooth'}); // For Chrome, Firefox, IE and Opera
 }
 
-function myFunction() {
-  document.getElementById("demo").innerHTML = "OPENING";
+// Aici este script-ul pentru butonul de login!
+function loginButton() {
+  setTimeout(
+    function() {
+      document.getElementById("demo").innerHTML = "Loading";
+  }, 1000);
+  setTimeout(
+    function() {
+      document.getElementById("demo").innerHTML = "Please";
+  }, 2000);
+  setTimeout(
+    function() {
+      document.getElementById("demo").innerHTML = "Wait";
+  }, 3000);
+
   setTimeout(
     function() {
       document.getElementById("demo").innerHTML = "LOGIN";
-      window.location.href = 'pages/404/index.html'
-  }, 1000);
+      // window.location.href = 'pages/404/index.html'
+      var txt;
+      var person = prompt("Please enter your key:", "");
+      if(person == null || person == "") return;
+      if(person == `5ZxvVidfYK84s3zYDgO0sAlfj7TLyvVRgB6DfpkWAsaW58nmHGKBVeTMGqKnICIg`) {
+        document.getElementById("demo").innerHTML = "Accepted";
+        setTimeout(
+          function() {
+            document.getElementById("demo").innerHTML = "LOGIN";
+            window.location.href = 'pages/404/index.html'
+        }, 5000);
+      } else {
+        document.getElementById("demo").innerHTML = "Declined";
+        setTimeout(
+          function() {
+            document.getElementById("demo").innerHTML = "LOGIN";
+        }, 5000);
+      }
+      // document.getElementById("demo").innerHTML = txt;
+  }, 5000);
 }
 
+// Aici este script-ul pentru butonul de go up!
 function loadTop() {
   setTimeout(
     function() {
       document.body.scrollTo({top: 0, behavior: 'smooth'}); // For Safari
       document.documentElement.scrollTo({top: 0, behavior: 'smooth'}); // For Chrome, Firefox, IE and Opera
+  }, 100);
+}
+
+// Aici este script-ul pentru butonul de chat!
+function chatBox() {
+  console.log('The mesaje box has been executed!')
+  var txt;
+  if (confirm(`Our website is not ready yet! Do you want to close this page?`)) {
+    window.location.href = 'index.html'
+  } else {
+    window.location.href = 'pages/404/index.html'
+  }
+  document.getElementById("demo").innerHTML = txt;
+  setTimeout(
+    function() {
+      window.location.href = 'pages/404/index.html'
   }, 100);
 }
